37220a85ac6c1ac9c5c180154eb0389be974f930
Gio Ciudadano <gaciudadano@up.edu.ph>
2023-08-11 00:16:25 +0800

    Add sample API response

--- server/utils/getEvents/index.js
+++ server/utils/getEvents/index.js
@@ -5,5 +5,18 @@ const getEvents = require('./getEvents');
 
 // eslint-disable-next-line
 module.exports = async ({ query }) => {
-  return 'Hakdog';
+  return {
+    event_name: 'Game Developers Summit 2023',
+    event_background: {
+      desktop: 'gamedev-desktop.jpg',
+      mobile: 'gamedev-mobile.jpg'
+    },
+    booths: [
+      'Game Engines',
+      'Monetization Practices',
+      'Music in Games',
+      'Collaborative Development',
+      'Games as a Service'
+    ]
+  };
 };
