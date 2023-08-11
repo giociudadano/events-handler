bac2036cc46e891f3576612e9d4b2c7af6cd8df1
Gio Ciudadano <gaciudadano@up.edu.ph>
2023-08-11 22:21:30 +0800

    Refactor BoothSelector

--- src/views/BoothSelectorView.vue
+++ src/views/BoothSelectorView.vue
@@ -1,31 +1,87 @@
 <template>
-  <div class="container">
-    <div class="about">
-      <br /><br /><br />
-      <div class="box">
-        Event Name: {{ eventName }} <br />
-        <label for="boothSelector">Select Booth:</label>
-        <div class="dropdown is-active">
-          <div class="dropdown-trigger">
-            <div class="select">
-              <select v-model="selectedBooth" id="boothSelector">
-                <option v-for="booth in booths" :value="booth" :key="booth">
-                  {{ booth }}
-                </option>
-              </select>
+  <div class="hero is-fullheight is-white-1">
+    <div class="hero-body hero-body-padding-small">
+      <div class="container">
+        <div class="columns is-centered is-vcentered">
+          <div class="column is-3">
+            <div class="box">
+              <div v-show="!views.dropdown">
+                <div class="loader is-loading"></div>
+              </div>
+              <div v-show="views.dropdown">
+                <label class="label is-small">Event Name</label>
+                <p>{{ event.event_name }}</p>
+              </div>
+              <!--
+              <br />
+              <label for="boothSelector">Select Booth:</label>
+              <div class="dropdown is-active">
+                <div class="dropdown-trigger">
+                  <div class="select">
+                    <select v-model="selectedBooth" id="boothSelector">
+                      <option
+                        v-for="booth in booths"
+                        :value="booth"
+                        :key="booth"
+                      >
+                        {{ booth }}
+                      </option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+              <button class="button is-primary">
+                <router-link :to="`/checker/${eventName}/${selectedBooth}`"
+                  >Go to Booth</router-link
+                >
+              </button>
+              -->
             </div>
           </div>
         </div>
-        <button class="button is-primary">
-          <router-link :to="`/checker/${eventName}/${selectedBooth}`"
-            >Go to Booth</router-link
-          >
-        </button>
       </div>
     </div>
   </div>
 </template>
 
+<style>
+.is-white-1 {
+  background-color: rgb(235, 235, 235);
+}
+</style>
+
+<script>
+import axios from 'axios';
+export default {
+  data() {
+    return {
+      params: {
+        event: null
+      },
+      event: {
+        event_name: null
+      },
+      views: {
+        dropdown: false
+      }
+    };
+  },
+  async mounted() {
+    this.params.event = this.$route.params.event;
+    this.event = await new Promise((resolve, reject) => {
+      axios
+        .get(`http://localhost:8081/api/getEvents?event=${this.params.event}`)
+        .then(response => {
+          this.views.dropdown = true;
+          resolve(response.data.data);
+        })
+        .catch(reject);
+    });
+  }
+};
+</script>
+
+<!--
 <script setup>
 import axios from 'axios';
 import { ref, onMounted } from 'vue';
@@ -37,7 +93,7 @@ const selectedBooth = ref('');
 onMounted(async () => {
   try {
     const response = await axios.get(
-      'http://localhost:8081/api/getEvents?event=Event%20A'
+      `http://localhost:8081/api/getEvents?event=Event%20A`
     ); // Replace with the actual API URL
     const data = response.data.data;
     eventName.value = data.event_name;
@@ -47,3 +103,4 @@ onMounted(async () => {
   }
 });
 </script>
+-->
